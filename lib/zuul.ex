19792f6c3042c98fe9269558fb80e537388eb903
Tristan Cacqueray <tdecacqu@redhat.com>
2021-11-08 23:21:06 +0000

    Initial summon

---
+++ lib/zuul.ex
@@ -0,0 +1,18 @@
+defmodule Zuul do
+  @moduledoc """
+  Documentation for `Zuul`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Zuul.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
